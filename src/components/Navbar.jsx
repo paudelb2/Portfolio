7c28e44c6cfb5dce83ee7229b89a355b2b653e63
paudelb2 <81331326+paudelb2@users.noreply.github.com>
2022-05-19 19:04:53 -0400

    portfolio 90% completion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,134 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Bipul from '../assets/Bipul.png'
+import {FaBars, FaTimes, FaGithub, FaLink, FaFacebook, FaLinkedin} from 'react-icons/fa'
+import {HiOutlineMail} from 'react-icons/hi'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
+import { Link } from 'react-scroll';
+
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
+
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#08192f] text-gray-300' ></div>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#08192f] text-gray-300 shadow-md shadow-[#040c16]'>
+      <div>
+        <img src={Bipul} alt='Logo Image' style={{width: '80px'}}/>
+      </div>
+
+
+  {/*menu*/}
+  <ul className='hidden md:flex'>
+        <li>
+          <Link to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to='work' smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+    {/*Hamburger menu*/}
+
+    <div onClick={handleClick} className='md:hidden z-10'>
+     {!nav ? <FaBars /> : <FaTimes />}
+    </div>
+
+    {/*Mobile menu*/}
+    <ul
+        className={
+          !nav
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+        }
+      >
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='work' smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+    {/*Social Icons*/}
+    <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+      <ul>
+        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500'>
+          <a className='flex justify-between items-center w-full text-gray-300'
+          href="https://www.linkedin.com/in/bipul-paudel">
+            LinkedIn <FaLinkedin size={30}/>
+          </a>
+        </li>
+        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+            <a className='flex justify-between items-center w-full text-gray-300'
+              href='https://github.com/paudelb2'>
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[brown]'>
+          <a className='flex justify-between items-center w-full text-gray-300'
+          href="/">
+            Resume <BsFillPersonLinesFill size={30}/>
+          </a>
+        </li>
+        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-500'>
+          <a className='flex justify-between items-center w-full text-gray-300'
+          href="mailto:paudelb2@protonmail.com">
+            Email <HiOutlineMail size={30}/>
+          </a>
+        </li>
+      </ul>
+
+    </div>
+
+
+
+
+
+
+    </div>
+    
   )
 }
 
